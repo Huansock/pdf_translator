a252e672e56cd63fd8bc48551dc5761b661d511e
wooseok yang <huan.sock@gmail.com>
2024-08-17 10:53:33 +0200

    connected with open ai visionw

--- pdf_translator/forms.py
+++ pdf_translator/forms.py
@@ -5,11 +5,12 @@ class TranslatorForm(forms.ModelForm):
     
     class Meta:
         model = Translator
-        fields = ('src_lang', 'dst_lang', 'pdf','pdf_name')
+        fields = ('src_lang', 'dst_lang', 'pdf','pdf_name', 'model_name')
         labels = {
             'src_lang': 'Source Language',
             'dst_lang': 'Destination Language',
             'pdf': 'PDF',
             'pdf_name': 'PDF Name',
+            'model_name':'AI model name'
         }
      
--- pdf_translator/models.py
+++ pdf_translator/models.py
@@ -2,9 +2,10 @@ from django.db import models
 # Create your models here.
 class Translator (models.Model):
     id = models.AutoField(blank=True,primary_key=True)
-    src_lang = models.CharField(max_length=255)
-    dst_lang = models.CharField(max_length=255)
-    pdf = models.FileField(upload_to='pdfs/')
-    pdf_name = models.CharField(max_length=1255)
-    md = models.FileField(null=True,blank=True,upload_to='mds/')
-    created_at = models.DateTimeField(null=True,blank=True,auto_now_add=True)
+    src_lang = models.CharField(max_length=255 , verbose_name="source language")
+    dst_lang = models.CharField(max_length=255, verbose_name='destination language')
+    pdf = models.FileField(upload_to='pdfs/',verbose_name="pdf")
+    pdf_name = models.CharField(max_length=1255 , verbose_name="pdf file name")
+    md = models.FileField(null=True,blank=True,upload_to='mds/' , verbose_name="markdown file")
+    created_at = models.DateTimeField(null=True,blank=True,auto_now_add=True , verbose_name="created_at")
+    model_name = models.CharField(choices=[("4o-mini","4o-mini"),("3-turbo","3-turbo")],verbose_name="model_name", null=True)
